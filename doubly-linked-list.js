b3ec30e2b024dbb44fed6f911442b86e4a777005
Brady Fukumoto <bradyfukumoto@gmail.com>
2021-04-01 16:11:33 -0700

    Linked List practice starter

---
+++ doubly-linked-list.js
@@ -0,0 +1,24 @@
+// Do not change this
+class DoublyLinkedListNode {
+  constructor(val) {
+    this.value = val;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    // Fill this in
+  }
+
+  addToHead(val) { // O(1)
+    // Fill this in
+  }
+
+  addToTail(val) { // O(1)
+    // Fill this in
+  }
+}
+
+module.exports = DoublyLinkedList;
---
+++ linked-list.js
@@ -0,0 +1,23 @@
+// Do not change this
+class LinkedListNode {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    // Fill this in
+  }
+
+  addToHead(val) {
+    // Fill this in
+  }
+
+  addToTail(val) {
+    // Fill this in
+  }
+}
+
+module.exports = LinkedList;
