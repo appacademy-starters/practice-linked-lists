71793e178f97abafe408e735d849452f14754fe6
Brady Fukumoto <bradyfukumoto@gmail.com>
2021-05-07 10:23:29 -0700

    Add debug print

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -19,6 +19,18 @@ class DoublyLinkedList {
   addToTail(val) { // O(1)
     // Fill this in
   }
+
+  // You can use this function to help debug
+  print() {
+    let current = this.head;
+
+    while (current) {
+      process.stdout.write(`${current.value} <-> `);
+      current = current.next;
+    }
+
+    console.log("NULL");
+  }
 }
 
 module.exports = DoublyLinkedList;
--- linked-list.js
+++ linked-list.js
@@ -18,6 +18,18 @@ class LinkedList {
   addToTail(val) {
     // Fill this in
   }
+
+  // You can use this function to help debug
+  print() {
+    let current = this.head;
+
+    while (current) {
+      process.stdout.write(`${current.value} -> `);
+      current = current.next;
+    }
+
+    console.log("NULL");
+  }
 }
 
 module.exports = LinkedList;
